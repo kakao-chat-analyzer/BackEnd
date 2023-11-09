51f92d62e8fecc650370bbe04265487978967c9c
goodyoung <goodyoung.goh@gmail.com>
2023-11-09 15:18:35 +0900

    Add: first-setting

---
+++ src/test/java/project/kakaochatanalyzer/KakaoChatAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.kakaochatanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoChatAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
