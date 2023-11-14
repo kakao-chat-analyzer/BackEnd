9d2a05ae6f8b355ba19a5d15b166f99b1d2b32e0
goodyoung <goodyoung.goh@gmail.com>
2023-11-14 16:40:24 +0900

    Add: modify MemberForm-2

--- src/main/java/project/kakaochatanalyzer/controller/MemberForm.java
+++ src/main/java/project/kakaochatanalyzer/controller/MemberForm.java
@@ -2,7 +2,7 @@ package project.kakaochatanalyzer.controller;
 
 public class MemberForm {
     /* HTML의 인자 값 name="name" 이랑 같이 매핑이 된다.
-    -> spring이 setname으로 설정해준다. 그럼 우린 getname으로 꺼내오면 된다.  modifiy*/
+    -> spring이 setname으로 설정해준다. 그럼 우린 getname으로 꺼내오면 된다.  modifiy2*/
     private String userId; // 아이디
     private String userPw; // 비밀번호
     private String userName; // 사용자 이름
