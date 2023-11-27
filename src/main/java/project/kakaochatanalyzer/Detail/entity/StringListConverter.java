c26a94ef4d3e0b2e8f29dec13ea7dc5e924a75a3
goodyoung <goodyoung.goh@gmail.com>
2023-11-28 04:58:54 +0900

    Add: StringListConverter

---
+++ src/main/java/project/kakaochatanalyzer/Detail/entity/StringListConverter.java
@@ -0,0 +1,23 @@
+package project.kakaochatanalyzer.Detail.entity;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+
+    private static final String DELIMITER = ",";
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        return String.join(DELIMITER, attribute);
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        return Arrays.asList(dbData.split(DELIMITER));
+    }
+}
